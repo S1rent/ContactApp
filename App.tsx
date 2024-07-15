3de962b169a311fe0fbf5ef095be808f2880caf6
Philip Indra Prayitno <philip.prayitno@gmail.com>
2024-07-15 20:22:46 +0700

    [Feature] Added Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import {NavigationContainer} from '@react-navigation/native';
@@ -9,9 +9,18 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import CreateScreen from './src/screens/create';
 import DetailScreen from './src/screens/detail';
 import UpdateScreen from './src/screens/update';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: React.FC = () => {
   const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <SafeAreaProvider>
